053f8b604e6b0fdc20a04ecb80fb82b74ac1cb9f
juleberry <105573843+juleberry@users.noreply.github.com>
2022-08-10 13:59:20 -0400

    add todays progress

--- functions.js
+++ functions.js
@@ -5,40 +5,89 @@
 // -------------------
 
 // PRINT GREETING
-const printGreeting = (name) => {
-  return `Hello there, ${name}!`
-}
-console.log(printGreeting('Slimer'));
+// const printGreeting = (name) => {
+//   return `Hello there, ${name}!`
+// }
+// console.log(printGreeting('Slimer'));
 
 
 // PRINT COOL
-const printCool = (name) => {
-  return `${name} is cool`
-}
-console.log(printCool('Captain Reynolds'));
+// const printCool = (name) => {
+//   return `${name} is cool`
+// }
+// console.log(printCool('Captain Reynolds'));
 
 // CALCULATE CUBE
-const calculateCube = (num) => {
-  return num * num * num
-}
-console.log(calculateCube(5));
+// const calculateCube = (num) => {
+//   return num * num * num
+// }
+// console.log(calculateCube(5));
 
 // IS VOWEL
-const isVowel = (char) => {
-  if (char.toLowerCase() === 'a' || char.toLowerCase() === 'e' || char.toLowerCase() === 'i' || char.toLowerCase() === 'o' || char.toLowerCase() === 'u') {
-    return true
-  } else {
-    return false
-  }
+// const isVowel = (char) => {
+//   if (char.toLowerCase() === 'a' || char.toLowerCase() === 'e' || char.toLowerCase() === 'i' || char.toLowerCase() === 'o' || char.toLowerCase() === 'u') {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// console.log(isVowel("A"))
+// console.log(isVowel("a"))
+// console.log(isVowel("E"))
+// console.log(isVowel("e"))
+// console.log(isVowel("I"))
+// console.log(isVowel("i"))
+// console.log(isVowel("O"))
+// console.log(isVowel("o"))
+// console.log(isVowel("U"))
+// console.log(isVowel("u"))
+// console.log(isVowel("Z"))
+
+// GET TWO LENGTHS
+// const getTwoLengths = (string1, string2) => {
+//   let lengthArr = [];
+//   lengthArr.push(string1.length, string2.length);
+//   return lengthArr
+// }
+// console.log(getTwoLengths('bird', 'turtle'));
+
+// GET MULTIPLE LENGTHS
+// const getMultipleLengths = (str) => {
+//   let arrOfNums = [];
+//   for(let i = 0; i < str.length; i++) {
+//     arrOfNums.push(str[i].length)
+//   }
+//   return arrOfNums;
+//   }
+
+// console.log(getMultipleLengths(["hello", "what", "is", "up", "dude"]));
+
+// MAX OF THREE
+// const maxOfThree = (num1, num2, num3) => {
+//   if (num1 === num2 && num2 === num3 && num1 === num3) {
+//     return num1
+//   } else if (num1 >= num2 && num1 >= num3) {
+//     return num1
+//   } else if (num2 >= num1 && num2 >= num3) {
+//     return num2
+//   } else {
+//     return num3
+//   }
+// }
+// all nums equal
+// console.log(maxOfThree(2, 2, 2));
+// should return num1
+// console.log(maxOfThree(9, 7, 5));
+// should return num2
+// console.log(maxOfThree(4, 7, 3));
+// should return num3
+// console.log(maxOfThree(3, 4, 7))
+// should return one of largest nums
+// console.log(maxOfThree(2, 3, 3))
+
+// PRINT LONGEST WORD -- incomplete
+const printLongestWord = (str) => {
+  let arrOfStrings = []
 }
-console.log(isVowel("A"))
-console.log(isVowel("a"))
-console.log(isVowel("E"))
-console.log(isVowel("e"))
-console.log(isVowel("I"))
-console.log(isVowel("i"))
-console.log(isVowel("O"))
-console.log(isVowel("o"))
-console.log(isVowel("U"))
-console.log(isVowel("u"))
-console.log(isVowel("Z"))
+
+// console.log(printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]));
